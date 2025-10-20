40561d4655ce82f3772605c3f170b3b28006f2b0
Rahul Kumar Reddy Lebaka lebaka <sai.lebaka@consultsda.com>
2025-10-20 13:42:55 +0000

    working block

--- app/adminapp/dist/Component-dbg.js
+++ app/adminapp/dist/Component-dbg.js
@@ -1,12 +1,12 @@
 sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+        ["sap/fe/core/AppComponent"],
+            function (Component) {
+                    "use strict";
 
-        return Component.extend("com.demo.admin.adminapp.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+                            return Component.extend("com.demo.admin.adminapp.Component", {
+                                        metadata: {
+                                                        manifest: "json"
+                                                                    }
+                                                                            });
+                                                                                }
+                                                                                );
--- srv/admin-service.cds
+++ srv/admin-service.cds
@@ -1,7 +1,6 @@
 using com.demo.expenses as db from '../db/schema';
 
 service AdminService @(requires: 'admin') {
-  // CORRECT SYNTAX FOR BOUND ACTIONS ON PROJECTION
 entity Expenses as projection on db.Expenses 
 actions {
       action approve();
